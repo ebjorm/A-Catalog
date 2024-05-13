f01fd89299a33fa936819fcc286f022d9583a328
ebjormae1 <erika.bjormans@aalto.fi>
2024-05-13 14:40:31 +0300

    React base set up + rough drafts of header and footer

---
+++ my-react-app/src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="app">
+      <h1>A Catalog.</h1>
+      <div className="view-switcher">
+        <button>Outfit</button>
+        <span>/</span>
+        <button>Grid</button>
+      </div>
+
+
+      {/* Render category components and other content here */}
+
+
+
+
+      <footer className="footer">
+      This is the footer.
+    </footer>
+    </div>
+
+
+  );
+};
+
+export default App;
