10dca1b15d27a8bd0342234008dad5d60e0a15f1
ebjormae1 <erika.bjormans@aalto.fi>
2024-05-24 09:03:17 +0300

    Arrows now have a fixed position

--- my-react-app/src/Outfit.js
+++ my-react-app/src/Outfit.js
@@ -13,26 +13,26 @@ const Outfit = ({ category, clothingItems, imageSize }) => {
   const items = Object.values(clothingItems[category]); //Puts items into an Array
   const currentItem = items[currentIndex]; //Gets the current item to be displayed based on currentIndex
 
-  //handels showing next item
+  //Handel showing the next item
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
   };
 
-  //handels showing previous item
+  //Handel showing the previous item
   const handlePrevious = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
   };
 
   return (
     <div className="category-outfit">
-      <h2>/n</h2>
+      <h2 style={{ visibility: 'hidden' }}>here to fix padding issue</h2> 
       <div className="outfit-item">
-        <button className="arrow-button" onClick={handlePrevious}>
-          <img src="/icons/Arrow.png" alt="Previous" className="left-arrow" />
+        <button className="arrow-button left-arrow" onClick={handlePrevious}>
+          <img src="/icons/ArrowL.png" alt="Previous" className="left-arrow" />
         </button>
         <img src={`/${currentItem.Image}`} alt={currentItem.Color} style={{ width: 'auto', height: imageSize }} />
         <button className="arrow-button" onClick={handleNext}>
-          <img src="/icons/Arrow.png" alt="Next" />
+          <img src="/icons/ArrowR.png" alt="Next" />
         </button>
       </div>
     </div>
